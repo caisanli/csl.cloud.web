f8c89e06d89e03acc0885cac225177100e281b63
CaisanLi <18081007254@sina.cn>
2020-11-05 21:52:50 +0800

    完善表格

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,18 +1,20 @@
-import React, { ReactNode, ReactNodeArray } from 'react';
+import React, { ReactElement, ReactNode, ReactNodeArray } from 'react';
 import Selecting from '@/components/Selecting';
-import { Checkbox, Menu } from 'antd';
+import { Checkbox } from 'antd';
 import styles from './index.module.less';
 interface IColumn {
   key: string; // 唯一值
   label: string; // 表头名称
   value: string; // 对应数据的key
   width?: string | number; // 列宽度
+  render?: (data: any) => ReactElement;
+  ellipsis?: boolean; // 是否显示省略号
   [key: string]: any;
 }
 interface IContextMenu {
   value: string;
   name: string;
-  icon?: ReactNode
+  icon?: ReactNode;
 }
 interface IProps {
   columns: IColumn[]; // 表格列的配置描述 {key, keyMap}
@@ -21,7 +23,8 @@ interface IProps {
   scrollSelector?: string | HTMLElement | Element; // 滚动区域元素
   select?: boolean; // 是否有选择框
   selecting?: boolean; // 是否框选
-  contextMenu?: IContextMenu[] | ((data: any) => IContextMenu[]) // 右键菜单
+  contextMenu?: IContextMenu[] | ((data: any) => IContextMenu[]); // 右键菜单
+  onClickContextMenu?: (item: IContextMenu, data: any) => void;
 }
 interface IState {
   checked: any[];
@@ -36,7 +39,7 @@ export default class Table extends React.Component<IProps, IState> {
       checked: [], // 记录选中的数据
       contextMenu: false, //
       contextMenuLeft: 0,
-      contextMenuTop: 0
+      contextMenuTop: 0,
     };
     // 绑定this
     this.onClickCheckAllBtn = this.onClickCheckAllBtn.bind(this);
@@ -61,7 +64,22 @@ export default class Table extends React.Component<IProps, IState> {
       checked,
     });
   }
-
+  // 监听框选
+  onSelect(selected: any[]) {
+    let checked: any[] = [];
+    selected.forEach(sel => {
+      checked.push(this.props.dataSource[sel.index]);
+    });
+    this.setState({
+      checked,
+    });
+  }
+  // 监听点击body
+  onClickBody() {
+    this.setState({
+      contextMenu: false,
+    });
+  }
   // 点击行
   onClickColumn(data: any) {
     let checked = this.state.checked;
@@ -71,72 +89,80 @@ export default class Table extends React.Component<IProps, IState> {
       checked.push(data);
     }
     this.setState({
-      checked
+      checked,
     });
   }
   // 右键菜单
   onContextMenu(data: any, e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     const contextMenu = this.props.contextMenu;
-    if(!contextMenu)
-      return ;
+    if (!contextMenu) return;
     const { clientX, clientY } = e;
     e.preventDefault();
     this.contextMenuData = data;
-    this.setState({
-      contextMenu: true
-    }, () => {
-      const elem = this.contextMenuRef.current;
-      this.setState({
-        contextMenuLeft: clientX,
-        contextMenuTop: clientY
-      })
-      console.log(this.contextMenuRef.current)
-    })
+    this.setState(
+      {
+        contextMenu: true,
+      },
+      () => {
+        const elem = this.contextMenuRef.current;
+        let bodyHeight = document.body.clientHeight;
+        let bodyWidth = document.body.clientWidth;
+        let elemWidth = elem?.clientWidth || 0;
+        let elemHeight = elem?.clientHeight || 0;
+        let left =
+          clientX + elemWidth > bodyWidth ? bodyWidth - elemWidth - 5 : clientX;
+        let top =
+          clientY + elemHeight > bodyHeight
+            ? bodyHeight - elemHeight - 5
+            : clientY;
+        this.setState({
+          contextMenuLeft: left,
+          contextMenuTop: top,
+        });
+      },
+    );
   }
-  // 监听框选
-  onSelect(selected: any[]) {
-    let checked: any[] = [];
-    selected.forEach(sel => {
-      checked.push(
-        this.props.dataSource[sel.index]
-      )
-    })
-    this.setState({
-      checked
-    })
+  // 点击右键菜单
+  onClickContextMenu(item: IContextMenu) {
+    if (typeof this.props.onClickContextMenu === 'function')
+      this.props.onClickContextMenu(item, this.contextMenuData);
   }
   // 生成右键菜单
   createContextMenu() {
     const contextMenus = this.props.contextMenu;
     const { contextMenuLeft, contextMenuTop, contextMenu } = this.state;
-    if(!contextMenu)
-      return null;
+    if (!contextMenu) return null;
     let child;
     let createMenu = (menu: IContextMenu[]) => (
-      <Menu>
-          {
-            menu.map((item: IContextMenu) => {
-              return <Menu.Item key={ item.value }>{ item.name }</Menu.Item>
-            })
-          }
-        </Menu>
-    )
-    if(contextMenus instanceof Array) {
+      <ul>
+        {menu.map((item: IContextMenu) => {
+          return (
+            <li onClick={() => this.onClickContextMenu(item)} key={item.value}>
+              <span className={styles.menuIcon}>{item.icon}</span>
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
+    );
+    if (contextMenus instanceof Array) {
       child = createMenu(contextMenus);
-    } else if(typeof contextMenus === 'function') {
+    } else if (typeof contextMenus === 'function') {
       child = createMenu(contextMenus(this.contextMenuData));
     }
     const style = {
       top: contextMenuTop + 'px',
-      left: contextMenuLeft + 'px'
-    }
+      left: contextMenuLeft + 'px',
+    };
     return (
-      <div className={styles.contextMenu} ref={ this.contextMenuRef } style={ style }>
-        <div className={ styles.contextMenuContent }>
-          { child }
-        </div>
+      <div
+        className={styles.contextMenu}
+        ref={this.contextMenuRef}
+        style={style}
+      >
+        <div className={styles.contextMenuContent}>{child}</div>
       </div>
-    )
+    );
   }
   // 生成表头
   createHead() {
@@ -160,7 +186,10 @@ export default class Table extends React.Component<IProps, IState> {
             return (
               <div
                 key={col.key}
-                className={styles.th}
+                className={[
+                  styles.th,
+                  col.ellipsis ? styles.ellipsis : '',
+                ].join(' ')}
                 style={this.columnWidth(col)}
               >
                 {col.label}
@@ -181,7 +210,7 @@ export default class Table extends React.Component<IProps, IState> {
               key={data[this.props.dataIndex]}
               className={styles.tr}
               onClick={() => this.onClickColumn(data)}
-              onContextMenu={ (e) => this.onContextMenu(data, e) }
+              onContextMenu={e => this.onContextMenu(data, e)}
             >
               {this.props.select ? (
                 <div className={[styles.td, styles.select].join(' ')}>
@@ -190,8 +219,14 @@ export default class Table extends React.Component<IProps, IState> {
               ) : null}
               {this.props.columns.map(col => {
                 return (
-                  <div key={col.key} className={styles.td}>
-                    {data[col.value]}
+                  <div
+                    key={col.key}
+                    className={[
+                      styles.td,
+                      col.ellipsis ? styles.ellipsis : '',
+                    ].join(' ')}
+                  >
+                    {col.render ? col.render(data) : data[col.value]}
                   </div>
                 );
               })}
@@ -203,12 +238,13 @@ export default class Table extends React.Component<IProps, IState> {
   }
   // 生成表格
   createTable() {
+    const fixed = !!this.props.columns.find(col => col.ellipsis);
     return (
-      <div className={styles.table}>
+      <div className={[styles.table, fixed ? styles.tableFixed : ''].join(' ')}>
         {this.createHead()}
         {this.createBody()}
       </div>
-    )
+    );
   }
   // 是否选中
   isChecked(data: any): boolean {
@@ -223,25 +259,30 @@ export default class Table extends React.Component<IProps, IState> {
     else width = defaultWidth;
     return { width };
   }
-
+  componentDidMount() {
+    document.addEventListener('click', this.onClickBody.bind(this));
+  }
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onClickBody.bind(this));
+  }
   render() {
-    if(this.props.selecting) {
+    if (this.props.selecting) {
       return (
         <Selecting
-          scrollSelector={ this.props.scrollSelector || '' }
+          scrollSelector={this.props.scrollSelector || ''}
           selector={`.${styles.tbody} .${styles.tr}`}
           onSelect={this.onSelect}
         >
-          { this.createTable() }
-          { this.createContextMenu() }
+          {this.createTable()}
+          {this.createContextMenu()}
         </Selecting>
       );
     }
     return (
       <>
-        { this.createTable() }
-        { this.createContextMenu() }
+        {this.createTable()}
+        {this.createContextMenu()}
       </>
-    )
+    );
   }
 }
