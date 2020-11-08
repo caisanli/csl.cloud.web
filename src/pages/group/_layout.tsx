1b32f67da8f68a1f56ebabfb84bd52f5023b94c2
CaisanLi <18081007254@sina.cn>
2020-11-08 21:17:38 +0800

    调整布局结构

--- src/pages/group/_layout.tsx
+++ src/pages/group/_layout.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import Layout from '@/components/Layouts';
+import { Layout } from 'antd';
+import CustomLayout, { Main } from '@/components/Layouts';
 import { IRouteComponentProps } from 'umi';
-
+const { Content } = Layout;
 export default (props: IRouteComponentProps) => {
-  return <Layout {...props} />;
+  if (props.location.pathname === '/group') {
+    return (
+      <Layout>
+        <Content>
+          <Main>{props.children}</Main>
+        </Content>
+      </Layout>
+    );
+  }
+  return <CustomLayout {...props} />;
 };
