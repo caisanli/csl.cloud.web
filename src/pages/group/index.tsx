21478e6850db7b0f99b0489a65dec768a9ca727e
CaisanLi <18081007254@sina.cn>
2020-11-08 21:17:56 +0800

    增加测试代码

--- src/pages/group/index.tsx
+++ src/pages/group/index.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
-
-export default function() {
-  return <div>Group Index</div>;
+import React, { FC } from 'react';
+import { history, ConnectProps, UserModelState, connect } from 'umi';
+interface IProps extends ConnectProps {
+  user: UserModelState;
 }
+const IndexPage: FC<IProps> = function(props) {
+  function goIndex() {
+    props.dispatch &&
+      props.dispatch({
+        type: 'user/updateGroup',
+        payload: {
+          info: { ...props.user },
+          group: 5,
+        },
+      });
+    history.push('/group/file');
+  }
+  return (
+    <div>
+      <button onClick={goIndex}>去首页</button>
+    </div>
+  );
+};
+const mapStateProps = state => ({
+  user: state.user.info,
+});
+export default connect(mapStateProps)(IndexPage);
