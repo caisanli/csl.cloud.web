9bd77a327ce6272723e7ab081d152578db57c889
CaisanLi <18081007254@sina.cn>
2020-11-08 21:17:15 +0800

    调整布局结构

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { IRouteComponentProps } from 'umi';
+import { Layout } from 'antd';
+import { Head } from '@/components/Layouts';
 import Login from '@/components/Login';
 import '@/assets/css/global.less';
+const { Header } = Layout;
 
 export default function(props: IRouteComponentProps) {
   if (props.location.pathname === '/login') {
     return <Login />;
   }
-  return props.children;
+  return (
+    <Layout>
+      <Header>
+        <Head />
+      </Header>
+      <Layout>{props.children}</Layout>
+    </Layout>
+  );
 }
