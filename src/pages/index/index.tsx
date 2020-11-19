9c9c6010272ac823ccca5975a267cfd7fcafa1e0
caigx <caigx@zxjmail.com>
2020-11-19 18:03:49 +0800

    添加文件上传测试代码

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -76,11 +76,50 @@ export default function Index() {
   // 重命名相关
   const [renameVisible, setRenameVisible] = useState(false);
   const renameForm = useRef();
+
+
+  const CHUNK_SIZE = 1024 * 1024 * 5;
+  function fileChange(e) {
+      const file = e.target.files[0];
+      const chunks = Math.ceil(file.size / CHUNK_SIZE);
+      upload(chunks, file)
+  }
+
+  function upload(total, file, index = 0) {
+      if(index >= total) {
+          alert('上传完成')
+          return ;
+      }
+      const formData = new FormData();
+      formData.append('name', file.name)
+      formData.append('size', file.size);
+      formData.append('chunks', total)
+      formData.append('chunk', (index + 1) + '');
+      formData.append('modifyTime', new Date(file.lastModifiedDate).getTime() + '')
+      let start = index * CHUNK_SIZE;
+      let end = CHUNK_SIZE * (index + 1);
+      end = end > file.size ? file.size : end;
+      console.log('start：', start)
+      console.log('end：', end)
+      formData.append('file', file.slice(start, end))
+      fetch('http://127.0.0.1:3000/apis/file/upload', {
+          method: 'POST',
+          body: formData,
+          credentials: 'same-origin'
+      }).then(res => {
+          upload(total, file, ++index)
+          console.log(res)
+      }).catch(err => {
+          console.log(err)
+      })
+  }
+
   return (
     <>
       <FileContainer dataSource={data} type="person" canCreateFolder onCreateFolder={ onCreateFolder } tools={tools} contextMenu={contextMenu} />
       <Folder ref={ folderForm } visible={ folderVisible } onSubmit={ onSubmitCreateFolder } onCancel={ () => setFolderVisible(false) } />
       <Rename ref={ renameForm } visible={ renameVisible } />
+      <input type="file" onChange={ fileChange } />
     </>
   );
 }
