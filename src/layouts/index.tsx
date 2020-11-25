8fd36eb14a0c5b05a4ed5994a0051ebd02026fed
caigx <caigx@zxjmail.com>
2020-11-25 17:56:13 +0800

    调整全局Layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,19 +4,33 @@ import { Layout } from 'antd';
 import { Head } from '@/components/Layouts';
 import Login from '@/components/Login';
 import '@/assets/css/global.less';
+import { INavItem } from '@/types';
 const { Header } = Layout;
+
+const nav: INavItem[] = [
+  {
+    name: '个人',
+    value: 'person',
+    path: '/',
+  },
+  {
+    name: '团队',
+    value: 'group',
+    path: '/group',
+  },
+];
 
 export default function(props: IRouteComponentProps) {
   const pathname = props.location.pathname;
   if (pathname === '/login') {
     return <Login />;
-  } else if (pathname.startsWith('/management')) {
+  } else if (pathname.startsWith('/admin')) {
     return props.children;
   }
   return (
     <Layout>
       <Header>
-        <Head />
+        <Head nav={ nav } />
       </Header>
       <Layout>{props.children}</Layout>
     </Layout>
