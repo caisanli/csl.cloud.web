9babdcfffcd93b59f16a03524915425a747bcd4b
caigx <caigx@zxjmail.com>
2020-11-27 17:53:48 +0800

    增加用户API

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,5 +4,17 @@ export default {
   // 查询用户
   query: (name?: string) => {
     return get('/user', { name })
+  },
+  // 创建用户
+  create: (name: string, phone?: string, email?: string) => {
+    return post('/user', { name, phone, email })
+  },
+  // 更新用户
+  update: (id: string, name: string, phone?: string, email?: string) => {
+    return put('/user/' + id, { name, phone, email })
+  },
+  // 根据ID获取信息
+  get(id: string) {
+    return get('/user/' + id);
   }
 }
