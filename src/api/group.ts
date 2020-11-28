eb343981051662c61004fca7b81c544bf7baf378
CaisanLi <18081007254@sina.cn>
2020-11-28 21:03:05 +0800

    修改API参数

--- src/api/group.ts
+++ src/api/group.ts
@@ -1,28 +1,34 @@
-import { get, post, del, put } from '@/utils/request'
+import { get, post, del, put } from '@/utils/request';
 
 export default {
   // 查询团队
   query(name?: string) {
-    return get('/group', { name })
+    return get('/group', { name });
   },
   // 创建团队
-  create(name: string, userId: string, size: number, description?: string) {
-    return post('/group', { name, userId, size, description })
+  create(name: string, user: string, size: number, description: string = '') {
+    return post('/group', { name, user, size, description });
   },
   // 更新团队
-  update(id: string, name: string, userId: string, size: number, description?: string) {
-    return put('/group/' + id, { name, userId, size, description })
+  update(
+    id: string,
+    name: string,
+    user: string,
+    size: number,
+    description: string = '',
+  ) {
+    return put('/group/' + id, { name, user, size, description });
   },
   // 删除团队
   remove(id: string) {
-    return del('/group/' + id)
+    return del('/group/' + id);
   },
   // 根据ID获取团队信息
   get(id: string) {
-    return get('/group/' + id)
+    return get('/group/' + id);
   },
   // 根据用户获取对应团队
   getByUser(userId: string) {
-    return get('/group/user/' + userId)
-  }
-}
+    return get('/group/user/' + userId);
+  },
+};
