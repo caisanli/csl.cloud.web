310d49a73631d88b76068cf4de67adef885fd2a3
CaisanLi <18081007254@sina.cn>
2020-11-28 21:03:41 +0800

    完成团队页面

--- src/pages/admin/group.tsx
+++ src/pages/admin/group.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { ColumnsType, IFormInstance, IGroup } from '@/types';
-import { Button, Col, Input, Row, Table } from '@/utils/_ant';
-import { UsergroupAddOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { ColumnsType, IFormInstance, IGroup, IUser } from '@/types';
+import { Button, Col, Input, message, Row, Table } from '@/utils/_ant';
+import {
+  UsergroupAddOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from '@ant-design/icons';
 import api from '@/api/group';
 import Modal from '@/components/Modal';
 import Form, { IFieldProps } from '@/components/Form';
 import SelectUser from '@/components/SelectUser';
 
 interface IState {
-  groups: IGroup[],
-  loading: boolean,
-  createUpdateVisible: boolean,
-  selectUserVisible: boolean
+  groups: IGroup[];
+  loading: boolean;
+  checkedUser: IUser | null;
+  createUpdateVisible: boolean;
+  selectUserVisible: boolean;
 }
 export default class Index extends React.Component<null, IState> {
   constructor(props: any) {
@@ -20,102 +25,190 @@ export default class Index extends React.Component<null, IState> {
       groups: [],
       loading: false,
       createUpdateVisible: false,
-      selectUserVisible: false
-    }
+      selectUserVisible: false,
+      checkedUser: null,
+    };
+    //
+    this.onSelectUser = this.onSelectUser.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   // 表单
-  form?: IFormInstance;
+  form?: IFormInstance<IGroup>;
 
   // 表格列数据
-  columns: ColumnsType<IGroup> = [{
-    title: '名称',
-    dataIndex: 'name'
-  }, {
-    title: '用户',
-    key: 'user',
-    render: (text: any, row: IGroup) => (
-      <>
-        { row.user?.name }
-      </>
-    ),
-  }, {
-    title: '总容量',
-    dataIndex: 'size'
-  }, {
-    title: '描述',
-    ellipsis: true,
-    dataIndex: 'description'
-  }, {
-    title: '操作',
-    width: '300px',
-    dataIndex: 'action',
-    render: () => (
-      <>
-        <Button size="small" icon={ <EditOutlined /> } style={{marginRight: '5px'}} />
-        <Button size="small" danger icon={ <DeleteOutlined /> } />
-      </>
-    )
-  }]
+  columns: ColumnsType<IGroup> = [
+    {
+      title: '名称',
+      dataIndex: 'name',
+    },
+    {
+      title: '用户',
+      key: 'user',
+      render: (text: any, row: IGroup) => <>{row.user?.name}</>,
+    },
+    {
+      title: '总容量',
+      dataIndex: 'size',
+    },
+    {
+      title: '描述',
+      ellipsis: true,
+      dataIndex: 'description',
+    },
+    {
+      title: '操作',
+      width: '300px',
+      dataIndex: 'action',
+      render: (text, row) => (
+        <>
+          <Button
+            onClick={() => this.onUpdate(row)}
+            size="small"
+            icon={<EditOutlined />}
+            style={{ marginRight: '5px' }}
+          />
+          {/* <Button onClick={ () => this.onDelete(row.id || '') } size="small" danger icon={ <DeleteOutlined /> } /> */}
+        </>
+      ),
+    },
+  ];
 
   // 表单数据
-  formField: IFieldProps[] = [{
-    key: 'name',
-    label: '名称',
-    rules: [
-      { required: true },
-      { whitespace: true },
-      { type: 'string', min: 2, max: 24 }
-    ],
-    input: {
-      placeholder: '输入名称'
-    }
-  }, {
-    key: 'size',
-    label: '容量',
-    input: {
-      customType: 'number',
-      placeholder: '输入容量',
-      formatter: value => value ? `${ value } M` : '',
-      parser: value => value ? value.replace(' M', '') : ''
-    }
-  }, {
-    key: 'userId',
-    label: '所有者',
-    input: () => (
-      <div>
-        <Input type="hidden" id="userId" />
-        <Button onClick={ () => this.setState({ selectUserVisible: true }) }>选择所有者</Button>
-      </div>
-    )
-  }, {
-    key: 'description',
-    label: '描述',
-    input: {
-      customType: 'textarea',
-      placeholder: '输入描述'
-    }
-  }]
+  formField: IFieldProps[] = [
+    {
+      key: 'name',
+      label: '名称',
+      rules: [
+        { required: true },
+        { whitespace: true },
+        { type: 'string', min: 2, max: 24 },
+      ],
+      input: {
+        placeholder: '输入名称',
+      },
+    },
+    {
+      key: 'size',
+      label: '容量',
+      rules: [{ required: true }],
+      input: {
+        customType: 'number',
+        placeholder: '输入容量',
+        formatter: value => (value ? `${value} M` : ''),
+        parser: value => (value ? value.replace(' M', '') : ''),
+      },
+    },
+    {
+      key: 'userId',
+      label: '所有者',
+      rules: [{ required: true }],
+      input: () => (
+        <div>
+          <Input type="hidden" id="userId" />
+          <Button onClick={() => this.setState({ selectUserVisible: true })}>
+            {this.state.checkedUser
+              ? this.state.checkedUser.name
+              : '选择所有者'}
+          </Button>
+        </div>
+      ),
+    },
+    {
+      key: 'description',
+      label: '描述',
+      input: {
+        customType: 'textarea',
+        placeholder: '输入描述',
+      },
+    },
+  ];
+
+  // 当前操作的id
+  id: string | null = null;
+
+  // 操作类型
+  type?: 'create' | 'update';
 
   // 搜索名称
   searchName?: string;
 
+  // 创建
   onCreate() {
+    this.type = 'create';
     this.setState({
-      createUpdateVisible: true
-    })
+      createUpdateVisible: true,
+    });
+  }
+
+  // 更新
+  onUpdate(group: IGroup) {
+    let { user, userId, ...other } = group;
+    let values = Object.assign({}, other, { userId: user?.id });
+    this.type = 'update';
+    this.id = group.id || null;
+    this.setState(
+      {
+        createUpdateVisible: true,
+        checkedUser: user || null,
+      },
+      () => {
+        this.form?.setFieldsValue(values);
+      },
+    );
+  }
+
+  // 删除
+  async onDelete(id: string) {
+    await api.remove(id);
+    message.success('删除成功');
+    this.query(this.searchName);
+  }
+
+  // 选中人员
+  onSelectUser(users: IUser[]) {
+    let user = users[0];
+    this.form?.setFieldsValue({
+      userId: user.id,
+    });
+    this.setState({
+      selectUserVisible: false,
+      checkedUser: user,
+    });
+  }
+
+  // 确认提交
+  onSubmit() {
+    this.form?.validateFields().then(async values => {
+      try {
+        let { name, size, userId, description } = values;
+        if (this.type === 'create') {
+          await api.create(name, userId, size, description);
+          message.success('添加成功');
+        } else if (this.id) {
+          await api.update(this.id, name, userId, size, description);
+          message.success('添加成功');
+        }
+        this.setState({
+          createUpdateVisible: false,
+        });
+        this.query(this.searchName);
+      } catch (error) {
+        message.error(error.message);
+      }
+    });
   }
 
   // 查询
   async query(name?: string) {
     this.setState({
-      loading: true
-    })
+      loading: true,
+    });
     const res = await api.query(name);
     this.setState({
       groups: res.data,
-      loading: false
-    })
+      loading: false,
+    });
   }
 
   componentDidMount() {
@@ -123,15 +216,22 @@ export default class Index extends React.Component<null, IState> {
   }
 
   render() {
-    let { groups, loading, createUpdateVisible, selectUserVisible } = this.state;
+    let {
+      groups,
+      loading,
+      createUpdateVisible,
+      selectUserVisible,
+      checkedUser,
+    } = this.state;
     return (
       <>
         <Row gutter={[0, 10]} align="middle" justify="space-between">
+          <Col>当前共{groups.length}个团队</Col>
           <Col>
-            当前共{ groups.length }个团队
-          </Col>
-          <Col>
-            <Button onClick={ () => this.onCreate() } icon={ <UsergroupAddOutlined /> }>
+            <Button
+              onClick={() => this.onCreate()}
+              icon={<UsergroupAddOutlined />}
+            >
               添加团队
             </Button>
             <Input.Search
@@ -143,27 +243,32 @@ export default class Index extends React.Component<null, IState> {
         </Row>
         <Table
           size="small"
-          loading={ loading }
-          rowKey='id'
-          pagination={ false }
-          columns={ this.columns }
-          dataSource={ groups }
+          loading={loading}
+          rowKey="id"
+          pagination={false}
+          columns={this.columns}
+          dataSource={groups}
         />
         {/* 创建/编辑团队 */}
         <Modal
           title="团队"
-          visible={ createUpdateVisible }
+          visible={createUpdateVisible}
+          onOk={this.onSubmit}
+          onCancel={() => this.setState({ createUpdateVisible: false })}
         >
           <Form
-            ref={ (form: IFormInstance) => this.form = form }
-            fieldData={ this.formField }
+            ref={(form: IFormInstance) => (this.form = form)}
+            fieldData={this.formField}
           />
         </Modal>
         {/* 选择人员 */}
         <SelectUser
-          visible={ selectUserVisible }
+          onOk={this.onSelectUser}
+          onCancel={() => this.setState({ selectUserVisible: false })}
+          checked={checkedUser ? [checkedUser.id || ''] : []}
+          visible={selectUserVisible}
         />
       </>
-    )
+    );
   }
 }
