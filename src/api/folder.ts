d2cc81802ce126eeadf4b860005538fffdfce36c
caigx <caigx@zxjmail.com>
2020-12-02 17:50:54 +0800

    添加文件夹接口

---
+++ src/api/folder.ts
@@ -0,0 +1,41 @@
+import { Result } from '@/types';
+import { get, post, put, del } from '@/utils/request';
+
+export default {
+  /**
+   * 创建
+   * @param {string} parentId
+   * @param {string} name
+   * @param {string} [description='']
+   * @returns {Promise<any>}
+   */
+  create(parentId: string, name: string, description: string = ''): Promise<Result> {
+    return post('/folder', { parentId, name, description });
+  },
+  /**
+   * 更新
+   * @param {string} id
+   * @param {string} name
+   * @param {string} [description='']
+   * @returns {Promise<Result>}
+   */
+  update(id: string, name: string, description: string = ''): Promise<Result> {
+    return put('/folder/' + id, { name, description })
+  },
+  /**
+   * 获取某个文件夹
+   * @param {string} id
+   * @returns {Promise<Result>}
+   */
+  getById(id: string): Promise<Result> {
+    return get('/folder/' + id);
+  },
+  /**
+   * 删除文件夹
+   * @param {string} id
+   * @returns {Promise<Result>}
+   */
+  remove(id: string): Promise<Result> {
+    return del('/folder/' + id);
+  }
+}
