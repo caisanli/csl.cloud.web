7e012a6fb8ea4a4a6e38d57e4e080dba3ff4afb0
CaisanLi <18081007254@sina.cn>
2020-12-02 21:51:31 +0800

    对接重命名接口

--- src/components/FileOperate/Rename.tsx
+++ src/components/FileOperate/Rename.tsx
@@ -1,30 +1,52 @@
-import React from 'react';
-import { Modal, Form, Input } from 'antd';
-import { IOperateProps } from "@/types";
-import { formLayout, modalProps, validateMessages } from './props';
-const rename = function(props: IOperateProps, ref) {
-  const [ form ] = Form.useForm();
+import React, { useState, useRef, useEffect } from 'react';
+import { message } from 'antd';
+import Form, { IFieldProps } from '@/components/Form';
+import Modal from '@/components/Modal';
+import { IFormInstance, IOperateProps } from '@/types';
+import api from '@/api/file';
+const fieldData: IFieldProps[] = [
+  {
+    key: 'name',
+    label: '名称',
+    rules: [{ required: true }],
+    input: {
+      placeholder: '输入名称',
+    },
+  },
+];
 
+export default function(props: IOperateProps) {
+  const [visible, setVisible] = useState<boolean>(false);
+  const { now, data, id, onSuccess } = props;
+  const form = useRef<IFormInstance>();
   function onSubmit() {
-    form.validateFields().then(props.onSubmit)
+    if (!form.current || !id) return;
+    form.current.validateFields().then(values => {
+      api.rename(id, values.name).then(() => {
+        setVisible(false);
+        message.success('修改成功');
+        onSuccess && onSuccess();
+      });
+    });
   }
+  function onCancel() {
+    setVisible(false);
+  }
+  useEffect(() => {
+    if (!now) return;
+    setVisible(true);
+    setTimeout(() => {
+      console.log(form);
+      if (data && form.current) {
+        form.current.setFieldsValue({
+          name: data.name,
+        });
+      }
+    }, 0);
+  }, [props.now]);
   return (
-    <Modal
-          title="重命名"
-          visible={ props.visible }
-          onOk={ onSubmit }
-          onCancel={ props.onCancel }
-          { ...modalProps }
-        >
-          <Form ref={ ref } form={ form } {...formLayout} validateMessages={ validateMessages }>
-            <Form.Item label="名称" name="name" rules={[
-              { required: true },
-              { type: 'string', min: 1, max: 24 }
-            ]}>
-              <Input placeholder="请输入名称" />
-            </Form.Item>
-          </Form>
-      </Modal>
+    <Modal title="重命名" visible={visible} onOk={onSubmit} onCancel={onCancel}>
+      <Form ref={form} fieldData={fieldData} />
+    </Modal>
   );
 }
-export default React.forwardRef(rename)
