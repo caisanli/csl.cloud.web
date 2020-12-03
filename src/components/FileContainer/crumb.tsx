e22fe6545f551b91615bd35148e28ca32bfa93dd
caigx <caigx@zxjmail.com>
2020-12-03 17:55:15 +0800

    调整面包屑组件

--- src/components/FileContainer/crumb.tsx
+++ src/components/FileContainer/crumb.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Divider } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 import { ConnectProps, connect, FileModelState } from 'umi';
@@ -6,7 +6,7 @@ import styles from './index.module.less';
 import { ICrumbItem } from '@/types';
 
 interface IProps {
-  crumbs: ICrumbItem[],
+  crumbs?: ICrumbItem[],
   fileCount?: number,
   folderCount?: number
 }
@@ -14,14 +14,25 @@ interface IProps extends ConnectProps {
   file: FileModelState
 }
 
-const Index: FC<IProps> = function (props: IProps) {
-  function onClickCrumb(item: ICrumbItem) {
+const Index = function (props: IProps) {
+  const { crumbs } = props;
+  function onClickCrumb(item: ICrumbItem, index: number) {
+    if(crumbs && crumbs.length - 1 === index)
+      return ;
+    dispatch(item.id);
+  }
+  // 返回上一级
+  function onBackPrev() {
+    const item = crumbs ? crumbs[crumbs.length - 2] : null;
+    item && dispatch(item.id);
+  }
+  function dispatch(id: string) {
     props.dispatch &&
     props.dispatch({
       type: 'file/setFolder',
       payload: {
         ...props.file,
-        folder: item.parentId
+        folder: id
       }
     })
   }
@@ -29,15 +40,28 @@ const Index: FC<IProps> = function (props: IProps) {
     <div className={styles.crumb}>
       <div className={styles.crumbLeft}>
         {
-          props.crumbs.length > 1
-            ? (<div className={styles.crumbItem}><span className={ styles.crumbItemName }>返回上一级</span><Divider type="vertical" /></div>)
-            : (<div className={ styles.crumbItem }><span className={ styles.crumbItemName }>全部文件</span><Divider type="vertical" /></div>)
+          crumbs && crumbs.length > 1
+            ? (<div className={styles.crumbItem}>
+                <span className={ styles.crumbItemName } onClick={ onBackPrev }>返回上一级</span>
+                <Divider type="vertical" />
+              </div>)
+            : (<div className={ styles.crumbItem }>
+                <span className={ styles.crumbItemName }>全部文件</span>
+                <Divider type="vertical" />
+              </div>)
         }
         {
-          props.crumbs.map(
-            item => (
-              <div key={item.id} className={ styles.crumbItem }>
-                <span onClick={ () => onClickCrumb(item) } className={ styles.crumbItemName }>{item.name}</span>
+          crumbs && crumbs.map(
+            (item, i) => (
+              <div key={ item.id }
+                className={ styles.crumbItem }
+              >
+                <span
+                  onClick={ () => onClickCrumb(item, i) }
+                  className={ styles.crumbItemName }
+                >
+                    { item.name }
+                </span>
                 <RightOutlined className={ styles.crumbItemIcon } />
               </div>
             )
