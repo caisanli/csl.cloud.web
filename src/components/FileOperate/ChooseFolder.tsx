836b6fdea7afc069181a2ecbff5011b52d80c15d
caigx <caigx@zxjmail.com>
2020-12-04 17:25:06 +0800

    完成选择目录组件

--- src/components/FileOperate/ChooseFolder.tsx
+++ src/components/FileOperate/ChooseFolder.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Tree } from 'antd';
 import Table, { IColumn } from '@/components/Table';
+import Scroll from '@/components/Scroll';
 import Modal from '@/components/Modal';
 import api from '@/api/folder';
 import { ICrumbItem, IFolder } from '@/types';
@@ -12,20 +13,28 @@ interface IProps {
   data?: any[];
   crumbs?: any[];
   onCrumbs?: (data: any) => void;
+  onOk?: (folder: string) => void;
+  onCancel?: () => void;
 }
+const iconStyle = {
+  fontSize: '18px',
+  fontWeight: 400,
+  verticalAlign: 'sub'
+};
 
 export default function(props: IProps) {
   const { visible, title } = props;
-  const [folder, setFolder] = useState<string>(props.folder || '0');
+
+  const [folder, setFolder] = useState<string>(props.folder || '');
   const [list, setList] = useState<IFolder[]>([]);
   const [crumbs, setCrumbs] = useState<ICrumbItem[]>([]);
-  //
+  // 表格项
   const columns: IColumn[] = [
     {
       key: 'icon',
       width: '35px',
       render() {
-        return <FolderOutlined style={{ fontSize: '18px' }} />;
+        return <FolderOutlined style={ iconStyle } />;
       },
     },
     {
@@ -38,10 +47,16 @@ export default function(props: IProps) {
       },
     },
   ];
+
+  useEffect(() => {
+    if (!visible) return;
+    setFolder('0');
+  }, [props.visible]);
+
   useEffect(() => {
     if (!visible) return;
     query();
-  }, [props.visible, folder]);
+  }, [folder]);
 
   async function query() {
     const {
@@ -50,19 +65,42 @@ export default function(props: IProps) {
     setList(folders);
     setCrumbs(crumbs);
   }
-  function onClickCrumb(item: ICrumbItem) {
+
+  function onClickCrumb(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, item: ICrumbItem) {
+    e.stopPropagation();
     setFolder(item.id);
   }
+
+  function onOk() {
+    props.onOk && props.onOk(folder);
+  }
+
   return (
-    <Modal title={title || '选择文件夹'} visible={visible}>
-      <Breadcrumb>
+    <Modal
+      title={ title || '选择文件夹' }
+      visible={ visible }
+      onOk={ onOk }
+      onCancel={ props.onCancel }
+    >
+      <Breadcrumb style={{ marginBottom: '5px' }}>
         {crumbs.map(item => (
-          <Breadcrumb.Item key={item.id} onClick={() => onClickCrumb(item)}>
-            {item.name}
+          <Breadcrumb.Item key={item.id}>
+            <a onClick={(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => onClickCrumb(e, item)}>{ item.name }</a>
           </Breadcrumb.Item>
         ))}
       </Breadcrumb>
-      <Table dataSource={list} dataIndex="id" columns={columns} />
+      <div style={{ height: '240px' }}>
+        <Scroll>
+          <Table
+            hiddenHead
+            dataSource={ list }
+            dataIndex="id"
+            columns={ columns }
+          />
+        </Scroll>
+
+      </div>
+
     </Modal>
   );
 }
