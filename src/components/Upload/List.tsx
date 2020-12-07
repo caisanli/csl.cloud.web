e29fd33f24ddfa4d3e15c7c68e711a73884f57e3
CaisanLi <18081007254@sina.cn>
2020-12-07 22:48:08 +0800

    添加上传进度

--- src/components/Upload/List.tsx
+++ src/components/Upload/List.tsx
@@ -1,4 +1,8 @@
-import { BorderOutlined, CloseOutlined, MinusOutlined } from '@ant-design/icons';
+import {
+  BorderOutlined,
+  CloseOutlined,
+  MinusOutlined,
+} from '@ant-design/icons';
 import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { Row, Col } from 'antd';
@@ -9,11 +13,18 @@ import styles from './index.module.less';
 import { IUploadFile } from '@/types';
 const defaultProps = {
   list: [],
-  message: ''
-}
-
+  message: '',
+};
+export type FileItem = {
+  name: string;
+  id: string;
+  process: number;
+  folder: string;
+  size: number;
+  status: 'pending' | 'waiting' | 'success' | 'error';
+};
 interface IProps {
-  list: IUploadFile[],
+  list: FileItem[];
   onRemove?: (file: IUploadFile) => void;
   message?: string;
 }
@@ -22,69 +33,77 @@ const doc = window.document;
 const node = doc.createElement('div');
 doc.body.appendChild(node);
 
-const columns:IColumn[] = [{
-  key: 'name',
-  label: '名称',
-  value: 'name',
-  width: '50%'
-}, {
-  key: 'size',
-  label: '大小',
-  render(data: any) {
-    return <>{ bytesToSize(data.size) }</>;
-  }
-}, {
-  key: 'folder',
-  label: '目录',
-  value: 'folder'
-}, {
-  key: 'process',
-  label: '进度',
-  render(data: any) {
-    return <>
-          {
-              data.status === 'pending'
-              ? data.process + '%'
-              : data.status === 'waiting'
-              ? '等待中'
-              : '已完成'
-            }
-    </>;
-  }
-}]
+const columns: IColumn[] = [
+  {
+    key: 'name',
+    label: '名称',
+    value: 'name',
+    width: '50%',
+    ellipsis: true,
+  },
+  {
+    key: 'size',
+    label: '大小',
+    render(data: any) {
+      return <>{bytesToSize(data.size)}</>;
+    },
+  },
+  {
+    key: 'folder',
+    label: '目录',
+    value: 'folder',
+  },
+  {
+    key: 'process',
+    label: '进度',
+    render(data: any) {
+      return (
+        <>
+          {data.status === 'pending'
+            ? data.process + '%'
+            : data.status === 'waiting'
+            ? '等待中'
+            : '已完成'}
+        </>
+      );
+    },
+  },
+];
 type Status = 'close' | 'min' | 'max';
 const Index = function(props: IProps) {
   let { message, list } = props;
   const [status, setStatus] = useState<Status>('max');
   useEffect(() => {
     setStatus('max');
-  }, [ props.list ])
+  }, [props.list]);
   return createPortal(
-    <div className={ styles.uploadBox }>
-      <div className={ styles.uploadContent }>
-        <Row justify="space-between" align="middle" className={ styles.uploadHead }>
-          <Col>
-            { message }
-          </Col>
-          <Col className={ styles.uploadTools }>
-            { status === 'min' && <BorderOutlined className={ styles.uploadTool } />}
-            { status === 'max' && <MinusOutlined className={ styles.uploadTool } /> }
-            <CloseOutlined className={ styles.uploadTool } />
+    <div className={styles.uploadBox}>
+      <div className={styles.uploadContent}>
+        <Row
+          justify="space-between"
+          align="middle"
+          className={styles.uploadHead}
+        >
+          <Col>{message}</Col>
+          <Col className={styles.uploadTools}>
+            {status === 'min' && (
+              <BorderOutlined className={styles.uploadTool} />
+            )}
+            {status === 'max' && (
+              <MinusOutlined className={styles.uploadTool} />
+            )}
+            <CloseOutlined className={styles.uploadTool} />
           </Col>
         </Row>
-        <div className={ styles.uploadBody }>
+        <div className={styles.uploadBody}>
           <Scroll>
-            <Table
-              dataIndex="id"
-              dataSource={ list }
-              columns={ columns }
-            />
+            <Table dataIndex="id" dataSource={list} columns={columns} />
           </Scroll>
         </div>
       </div>
     </div>,
-    node
-  )
-}
+    node,
+  );
+};
 Index.defaultProps = defaultProps;
 export default Index;
--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,65 +1,81 @@
 import React, { useEffect, useState } from 'react';
 import { post } from '@/utils/request';
+import useRefState from '@/hooks/useRefState';
 import { Button } from 'antd';
 import List from './List';
 import { uuid } from '@/utils';
 import styles from './index.module.less';
 import { IUploadFile } from '@/types';
-
+import { FileItem } from './List';
 const defaultProps = {
   fileName: 'file',
   chunkSize: 4 * 1024 * 1024, // 4M
   action: '',
-  data: {}
-}
+  data: {},
+};
 export interface IUploadProps {
   children?: React.ReactNode;
   fileName?: string;
   action?: string;
   chunkSize?: number;
   data?: {
-    [key:string]: any
+    [key: string]: any;
   };
   style?: React.CSSProperties;
   onSuccess?: (data: any) => void;
+}
+type State = {
+  list: FileItem[];
 };
 
-const Index = function(props: IUploadProps & typeof defaultProps) {
-  const { chunkSize, onSuccess, action, fileName, data } = props;
-  const [ list, setList ] = useState<IUploadFile[]>([])
-  const CHUNK_SIZE = chunkSize;
-
-  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
-    if(!e || !e.target || !e.target.files)
-      return ;
-    addFileList([...e.target.files]);
+class Index extends React.Component<IUploadProps, State> {
+  constructor(props: IUploadProps) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+    this.onChange = this.onChange.bind(this);
+    this.CHUNK_SIZE = props.chunkSize!;
   }
 
-  useEffect(() => {
-    console.log('更新后的：',list)
-  }, [list])
+  CHUNK_SIZE: number;
 
-  function addFileList(files: IUploadFile[]) {
-    let successFiles: IUploadFile[] = [];
+  static defaultProps = defaultProps;
+
+  onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e || !e.target || !e.target.files) return;
+    this.addFileList([...e.target.files]);
+  }
+
+  addFileList(files: IUploadFile[]) {
+    let successFiles: FileItem[] = [];
     let uFiles: IUploadFile[] = [];
     files.forEach(file => {
-      if(!testFile(file))
-        return ;
-      handleFile(file);
+      if (!this.testFile(file)) return;
+      this.handleFile(file);
       uFiles.push(file);
-      successFiles.push(file);
-    })
-    setList(successFiles)
-    uploadFiles(uFiles)
+      successFiles.push({
+        name: file.name,
+        id: file.id || '',
+        folder: '根目录',
+        size: file.size,
+        process: 0,
+        status: 'waiting',
+      });
+    });
+    this.setState({
+      list: this.state.list.concat(successFiles),
+    });
+    this.uploadFiles(uFiles);
   }
 
   // 检验文件
-  function testFile(file: IUploadFile):boolean {
+  testFile(file: IUploadFile): boolean {
     return true;
   }
 
   // 处理文件
-  function handleFile(file: IUploadFile) {
+  handleFile(file: IUploadFile) {
     file.id = uuid();
     file.process = 0;
     file.folder = '';
@@ -67,80 +83,108 @@ const Index = function(props: IUploadProps & typeof defaultProps) {
   }
 
   // 上传文件列表
-  function uploadFiles(files: IUploadFile[]) {
+  uploadFiles(files: IUploadFile[]) {
     const file = files.shift();
-    if(file) {
+    if (file) {
       file.status = 'pending';
-      uploadBefore(file, () => {
-        uploadFiles(files);
+      this.uploadBefore(file, () => {
+        this.uploadFiles(files);
       });
     } else {
-      console.log('全部上传完成...')
+      console.log('全部上传完成...');
     }
   }
 
+  setFileStatus(id: string = '', value: any) {
+    let index = 0,
+      newList = [...this.state.list];
+    let activeItem = newList.find((item, i) => {
+      index = i;
+      return item.id === id;
+    });
+    if (!activeItem) return;
+    let newItem = Object.assign({}, activeItem, value);
+    newList.splice(index, 1, newItem);
+    this.setState({
+      list: newList,
+    });
+  }
+
   // 上传前
-  function uploadBefore(file: IUploadFile, callback: () => void) {
-    const chunks = Math.ceil(file.size / CHUNK_SIZE);
-    upload(chunks, file, 0, callback)
+  uploadBefore(file: IUploadFile, callback: () => void) {
+    const chunks = Math.ceil(file.size / this.CHUNK_SIZE);
+    this.upload(chunks, file, 0, callback);
   }
 
   // 上传
-  function upload(total: number, file: IUploadFile, index = 0, callback: () => void) {
+  upload(total: number, file: IUploadFile, index = 0, callback: () => void) {
+    let { data = {}, fileName = '', action = '', onSuccess } = this.props;
     const formData = new FormData();
     Object.keys(data).forEach(key => {
       formData.append(key, data[key]);
-    })
-    formData.append('name', file.name)
+    });
+    formData.append('name', file.name);
     formData.append('size', file.size + '');
-    formData.append('chunks', total + '')
-    formData.append('chunk', (index + 1) + '');
+    formData.append('chunks', total + '');
+    formData.append('chunk', index + 1 + '');
     formData.append('modifyDate', new Date(file.lastModified).getTime() + '');
-    let start = index * CHUNK_SIZE;
-    let end = CHUNK_SIZE * (index + 1);
+    let start = index * this.CHUNK_SIZE;
+    let end = this.CHUNK_SIZE * (index + 1);
     end = end > file.size ? file.size : end;
-    formData.append(fileName, file.slice(start, end))
-    post(action, formData).then(res => {
-      if (res.code === 1) {
-        index++;
-        if (index >= total) { // 所有分片上传完成
-          file.process = 100;
-          file.status = 'success';
-          onSuccess && onSuccess(res);
-          callback();
-          return;
+    formData.append(fileName, file.slice(start, end));
+    post(action, formData)
+      .then(res => {
+        if (res.code === 1) {
+          index++;
+          if (index >= total) {
+            // 所有分片上传完成
+            file.process = 100;
+            file.status = 'success';
+            this.setFileStatus(file.id, {
+              status: 'success',
+              process: 100,
+            });
+            onSuccess && onSuccess(res);
+            callback();
+            return;
+          }
+          file.process = res.data.process * 100;
+          this.setFileStatus(file.id, {
+            process: file.process,
+            status: 'pending',
+          });
+          this.upload(total, file, index, callback);
         }
-        file.process = (res.data.process * 100);
-        upload(total, file, index, callback);
-      }
-    }).catch(err => {
-      file.status = 'error';
-      console.log(err)
-    })
+      })
+      .catch(err => {
+        file.status = 'error';
+        console.log(err);
+      });
   }
 
-  return (
-    <div
-      className={ styles.upload }
-      style={ props.style }
-    >
-      {
-        props.children
-        ? props.children
-        : <Button size="small" type="primary">上传</Button>
-      }
-      <input
-        onChange={ onChange }
-        className={ styles.uploadInput }
-        type="file"
-        multiple
-      />
-      <List
-        list={ list }
-        message="已完成"
-      />
-    </div>
-  )
+  render() {
+    let props = this.props;
+    let { list } = this.state;
+    return (
+      <div className={styles.upload} style={props.style}>
+        {props.children ? (
+          props.children
+        ) : (
+          <Button size="small" type="primary">
+            上传
+          </Button>
+        )}
+        <input
+          onChange={this.onChange}
+          className={styles.uploadInput}
+          type="file"
+          multiple
+        />
+        <List list={list} message="已完成" />
+      </div>
+    );
+  }
 }
+
 Index.defaultProps = defaultProps;
 export default Index;
