871700208a39fc7b56f5dc0609203dce78f271d4
caigx <caigx@zxjmail.com>
2020-12-07 17:39:44 +0800

    增加上传列表

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { post } from '@/utils/request';
-import {createPortal} from 'react-dom';
 import { Button } from 'antd';
+import List from './List';
+import { uuid } from '@/utils';
+import styles from './index.module.less';
+import { IUploadFile } from '@/types';
 
 const defaultProps = {
   fileName: 'file',
@@ -21,69 +24,78 @@ export interface IUploadProps {
   onSuccess?: (data: any) => void;
 };
 
-import styles from './index.module.less';
-const doc = window.document;
-const node = doc.createElement('div');
-doc.body.appendChild(node);
 const Index = function(props: IUploadProps & typeof defaultProps) {
   const { chunkSize, onSuccess, action, fileName, data } = props;
+  const [ list, setList ] = useState<IUploadFile[]>([])
   const CHUNK_SIZE = chunkSize;
 
   function onChange(e: React.ChangeEvent<HTMLInputElement>) {
     if(!e || !e.target || !e.target.files)
       return ;
     addFileList([...e.target.files]);
-
   }
-  function addFileList(files: File[]) {
-    let successFiles:File[] = []
+
+  useEffect(() => {
+    console.log('更新后的：',list)
+  }, [list])
+
+  function addFileList(files: IUploadFile[]) {
+    let successFiles: IUploadFile[] = [];
+    let uFiles: IUploadFile[] = [];
     files.forEach(file => {
       if(!testFile(file))
         return ;
       handleFile(file);
+      uFiles.push(file);
       successFiles.push(file);
     })
-    uploadFiles(successFiles)
+    setList(successFiles)
+    uploadFiles(uFiles)
   }
+
   // 检验文件
-  function testFile(file: File):boolean {
+  function testFile(file: IUploadFile):boolean {
     return true;
   }
 
   // 处理文件
-  function handleFile(file: File) {
-
+  function handleFile(file: IUploadFile) {
+    file.id = uuid();
+    file.process = 0;
+    file.folder = '';
+    file.status = 'waiting';
   }
 
   // 上传文件列表
-  function uploadFiles(files: File[]) {
+  function uploadFiles(files: IUploadFile[]) {
     const file = files.shift();
     if(file) {
+      file.status = 'pending';
       uploadBefore(file, () => {
         uploadFiles(files);
       });
     } else {
-      alert('全部上传完成...')
+      console.log('全部上传完成...')
     }
   }
 
   // 上传前
-  function uploadBefore(file: File, callback: () => void) {
+  function uploadBefore(file: IUploadFile, callback: () => void) {
     const chunks = Math.ceil(file.size / CHUNK_SIZE);
     upload(chunks, file, 0, callback)
   }
 
   // 上传
-  function upload(total, file, index = 0, callback: () => void) {
+  function upload(total: number, file: IUploadFile, index = 0, callback: () => void) {
     const formData = new FormData();
     Object.keys(data).forEach(key => {
       formData.append(key, data[key]);
     })
     formData.append('name', file.name)
-    formData.append('size', file.size);
-    formData.append('chunks', total)
+    formData.append('size', file.size + '');
+    formData.append('chunks', total + '')
     formData.append('chunk', (index + 1) + '');
-    formData.append('modifyDate', new Date(file.lastModifiedDate).getTime() + '')
+    formData.append('modifyDate', new Date(file.lastModified).getTime() + '');
     let start = index * CHUNK_SIZE;
     let end = CHUNK_SIZE * (index + 1);
     end = end > file.size ? file.size : end;
@@ -92,14 +104,17 @@ const Index = function(props: IUploadProps & typeof defaultProps) {
       if (res.code === 1) {
         index++;
         if (index >= total) { // 所有分片上传完成
-          // alert('上传完成')
+          file.process = 100;
+          file.status = 'success';
           onSuccess && onSuccess(res);
           callback();
           return;
         }
-        upload(total, file, index, callback)
+        file.process = (res.data.process * 100);
+        upload(total, file, index, callback);
       }
     }).catch(err => {
+      file.status = 'error';
       console.log(err)
     })
   }
@@ -118,6 +133,11 @@ const Index = function(props: IUploadProps & typeof defaultProps) {
         onChange={ onChange }
         className={ styles.uploadInput }
         type="file"
+        multiple
+      />
+      <List
+        list={ list }
+        message="已完成"
       />
     </div>
   )
