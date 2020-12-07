ca383ecd25c6b55ab7ae69788dac1c52afcd1e4a
CaisanLi <18081007254@sina.cn>
2020-12-07 22:46:45 +0800

    添加hook

---
+++ src/hooks/useRefState.ts
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const useRefState = <T>(
+  initialValue: T,
+): [T, React.MutableRefObject<T>, React.Dispatch<React.SetStateAction<T>>] => {
+  const [state, setState] = useState<T>(initialValue);
+  const stateRef = useRef(state);
+  useEffect(() => {
+    stateRef.current = state;
+  }, [state]);
+  return [state, stateRef, setState];
+};
+export default useRefState;
