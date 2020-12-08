6bddf76447818c00a90f29cc16fa04840cfb768d
CaisanLi <18081007254@sina.cn>
2020-12-08 22:08:43 +0800

    修改查询接口

--- src/api/file.ts
+++ src/api/file.ts
@@ -12,8 +12,15 @@ export default {
    * @param {string} [name] 文件名称
    * @returns
    */
-  query(folderId: string, sort: SORT, order: ORDER, page: number, num: number, name?: string) {
-    return get('/file', { folderId, sort, order, page, num })
+  query(
+    folderId: string,
+    sort: SORT,
+    order: ORDER,
+    page: number,
+    num: number,
+    name?: string,
+  ) {
+    return get('/file', { folderId, sort, order, page, num, name });
   },
   /**
    * 根据ID获取文件
@@ -21,7 +28,7 @@ export default {
    * @returns
    */
   getById(id: string) {
-    return get('/file/' + id)
+    return get('/file/' + id);
   },
   /**
    * 重命名文件
@@ -30,7 +37,7 @@ export default {
    * @returns
    */
   rename(id: string, name: string) {
-    return put('/file/rename/' + id, { name })
+    return put('/file/rename/' + id, { name });
   },
   /**
    * 上传文件
@@ -43,7 +50,15 @@ export default {
    * @param {number} chunks 总切片数量
    * @returns
    */
-  upload(folder: string, file: any, name: string, size: number, modifyDate: number, chunk: number, chunks: number) {
+  upload(
+    folder: string,
+    file: any,
+    name: string,
+    size: number,
+    modifyDate: number,
+    chunk: number,
+    chunks: number,
+  ) {
     let formData = new FormData();
     formData.append('folder', folder);
     formData.append('name', name);
@@ -52,7 +67,7 @@ export default {
     formData.append('chunk', chunk + '');
     formData.append('chunks', chunks + '');
     formData.append('file', file);
-    return post('/file/update', formData)
+    return post('/file/update', formData);
   },
   /**
    * 获取切片文件上传进度
@@ -61,7 +76,7 @@ export default {
    * @param {number} modifyDate 最后更新时间
    */
   getChunkProcess(name: string, size: number, modifyDate: number) {
-    return get('/file/chunk/process', { name, size, modifyDate })
+    return get('/file/chunk/process', { name, size, modifyDate });
   },
   /**
    * 批量删除文件、文件夹
@@ -70,7 +85,7 @@ export default {
    * @returns
    */
   remove(fileIds: string = '', folderIds: string = '') {
-    return del('/file',{ fileIds, folderIds })
+    return del('/file', { fileIds, folderIds });
   },
   /**
    * 移动文件
@@ -79,7 +94,7 @@ export default {
    * @returns
    */
   move(ids: string, folderId: string) {
-    return put('/file/move', { ids, folderId })
+    return put('/file/move', { ids, folderId });
   },
   /**
    * 复制文件
@@ -88,7 +103,7 @@ export default {
    * @returns
    */
   copy(ids: string, folderId: string) {
-    return put('/file/copy', { ids, folderId })
+    return put('/file/copy', { ids, folderId });
   },
   /**
    * 下载文件
@@ -96,6 +111,6 @@ export default {
    * @returns
    */
   download(files: string) {
-    return get('/file/folder/download', { files })
-  }
-}
+    return get('/file/folder/download', { files });
+  },
+};
