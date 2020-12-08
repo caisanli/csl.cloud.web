d86e897c22a782634218faf2220bca19177d18a9
CaisanLi <18081007254@sina.cn>
2020-12-08 22:10:24 +0800

    增加文件操作

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -7,19 +7,25 @@ import {
   DeleteOutlined,
   EditOutlined,
   CopyOutlined,
-  ScissorOutlined
+  ScissorOutlined,
 } from '@ant-design/icons';
 import { IContextMenu, ICrumbItem, IPage, IToolBar } from '@/types';
 import fileApi from '@/api/file';
 import Move from '@/components/FileOperate/Move';
-import { copyBtn, delBtn, downBtn, moveBtn, renameBtn, shareBtn } from '@/components/FileContainer/tools';
+import {
+  copyBtn,
+  delBtn,
+  downBtn,
+  moveBtn,
+  renameBtn,
+  shareBtn,
+} from '@/components/FileContainer/tools';
 
 interface IProps extends ConnectProps {
   file: FileModelState;
 }
 
-const Index = function (props: IProps) {
-
+const Index = function(props: IProps) {
   // 文件夹菜单
   const folderRightMenu: IContextMenu[] = [
     {
@@ -43,7 +49,7 @@ const Index = function (props: IProps) {
       name: '分享',
       value: 'share',
       icon: <ShareAltOutlined />,
-      onClick(data: any) { },
+      onClick(data: any) {},
     },
   ];
   // 文件右键菜单
@@ -62,51 +68,59 @@ const Index = function (props: IProps) {
       name: '删除',
       value: 'delete',
       icon: <DeleteOutlined />,
-      onClick(data: any) { },
+      onClick(data: any) {
+        del(data.id, '', onSuccess);
+      },
     },
     {
       name: '分享',
       value: 'share',
       icon: <ShareAltOutlined />,
-      onClick(data: any) { },
+      onClick(data: any) {},
     },
     {
       name: '移动到',
       value: 'move',
       icon: <ScissorOutlined />,
-      onClick(data: any) { },
+      onClick(data: any) {
+        setMoveIds(data.id);
+        move('move');
+      },
     },
     {
       name: '复制到',
       value: 'copy',
       icon: <CopyOutlined />,
-      onClick(data: any) { },
+      onClick(data: any) {
+        setMoveIds(data.id);
+        move('copy');
+      },
     },
   ];
 
   // 自定义生成右键菜单
-  const contextMenu = function (data: any): IContextMenu[] {
+  const contextMenu = function(data: any): IContextMenu[] {
     if (data.parentId) return folderRightMenu;
     else return fileRightMenu;
   };
 
   const [crumbs, setCrumbs] = useState<ICrumbItem[]>([]);
   const [list, setList] = useState([]);
-  const [page, setPage] = useState<IPage>()
+  const [page, setPage] = useState<IPage>();
   const {
     folder,
+    name,
     sort: { order, type },
-    no
+    no,
   } = props.file;
   async function query() {
     const {
       data: { crumbs, files, folders, page },
-    } = await fileApi.query(folder, type, order, no, 10);
-    // console.log('page：', page)
+    } = await fileApi.query(folder, type, order, no, 10, name);
     setPage(page);
     setCrumbs(crumbs);
-    setTools([])
-    if(no === 1) {
+    setTools([]);
+    if (no === 1) {
       setList(folders.concat(files));
     } else {
       setList(list.concat(files));
@@ -115,9 +129,7 @@ const Index = function (props: IProps) {
   // 文件夹
   const [fceDate, setFceDate] = useState<number>();
   const [fId, setFId] = useState<string>();
-  function onCreateFolder() {
-    setFceDate(Date.now());
-  }
+
   // 文件
   const [renameDate, setRenameDate] = useState<number>();
   const [fileId, setFileId] = useState<string>();
@@ -125,33 +137,32 @@ const Index = function (props: IProps) {
   const [selected, setSelected] = useState<any[]>([]);
   // 操作完成的回调
   function onSuccess() {
-    dispatchNo(1);
+    dispatch({
+      no: 1,
+    });
   }
-  function dispatchNo(no: number) {
-    props.dispatch && props.dispatch({
-      type: 'file/setNo',
-      payload: {
-        ...props.file,
-        no
-      }
-    })
-  }
+
   // dispatch
-  function dispatchFolder(folder: string) {
-    props.dispatch && props.dispatch({
-      type: 'file/setFolder',
-      payload: {
-        ...props.file,
-        folder
-      }
-    })
+  function dispatch(data: any) {
+    props.dispatch &&
+      props.dispatch({
+        type: 'file/update',
+        payload: {
+          ...props.file,
+          ...data,
+        },
+      });
   }
   // 点击项
   function onClickColumn(data: any) {
-    if (data.parentId) { // 点击了文件夹
-      dispatchFolder(data.id);
-    } else { // 点击了文件
-
+    if (data.parentId) {
+      // 点击了文件夹
+      dispatch({
+        folder: data.id,
+        no: 1,
+      });
+    } else {
+      // 点击了文件
     }
   }
 
@@ -161,19 +172,21 @@ const Index = function (props: IProps) {
   function onSelect(data: any[]) {
     let type: number = 0;
 
-    for(let i = 0; i < data.length; i++ ) {
+    for (let i = 0; i < data.length; i++) {
       const d = data[i];
-      if(type === 3) break ;
-      if(d.parentId) { // 文件夹
+      if (type === 3) break;
+      if (d.parentId) {
+        // 文件夹
         type = type === 1 ? 3 : 2;
-      } else { // 文件
+      } else {
+        // 文件
         type = type === 2 ? 3 : 1;
       }
     }
     setSelected(data);
-    let tools:IToolBar[] = [];
-    switch(type) {
-      case 1 : // 文件
+    let tools: IToolBar[] = [];
+    switch (type) {
+      case 1: // 文件
         tools = [shareBtn, downBtn, delBtn, renameBtn, copyBtn, moveBtn];
         break;
       case 2: // 文件夹
@@ -188,9 +201,10 @@ const Index = function (props: IProps) {
   // 点击工具栏
   function onClickTool(tool: IToolBar) {
     let idsObj = getSelectedIds();
-    switch(tool.type) {
+    switch (tool.type) {
       case 'move':
       case 'copy':
+        setMoveIds(idsObj.files);
         move(tool.type);
         break;
       case 'delete':
@@ -199,52 +213,48 @@ const Index = function (props: IProps) {
     }
   }
   // 获取
-  function getSelectedIds(): { files: string, folders: string } {
+  function getSelectedIds(): { files: string; folders: string } {
     let files: string[] = [],
-        folders: string[] = [];
+      folders: string[] = [];
     selected.forEach(sel => {
-      if(sel.parentId)
-        folders.push(sel.id)
-      else
-        files.push(sel.id);
-    })
-    return { files: files.join(','), folders: folders.join(',') }
+      if (sel.parentId) folders.push(sel.id);
+      else files.push(sel.id);
+    });
+    return { files: files.join(','), folders: folders.join(',') };
   }
-  const [ moveDate, setMoveDate ] = useState<number>();
-  const [ moveIds, setMoveIds ] = useState<string>();
-  const [ moveType, setMoveType ] = useState<string>();
+  const [moveDate, setMoveDate] = useState<number>();
+  const [moveIds, setMoveIds] = useState<string>();
+  const [moveType, setMoveType] = useState<string>();
   function move(type: string) {
-    let ids: string[] = [];
-    selected.forEach(d => {
-      ids.push(d.id);
-    })
     setMoveType(type);
-    setMoveIds(ids.join(','));
     setMoveDate(Date.now());
   }
 
   function onMoveSuccess(folder: string) {
-    dispatchFolder(folder);
+    dispatch({
+      folder,
+    });
   }
 
   let scrollTimer: NodeJS.Timeout;
   function onScroll(data: any) {
-    console.log('scroll...')
-    if(scrollTimer)
-      clearTimeout(scrollTimer);
+    if (scrollTimer) clearTimeout(scrollTimer);
     scrollTimer = setTimeout(() => {
-      console.log('page：', page)
-      if(!page) return ;
-      if(data.isBottom && page.page < page.count) {
-        console.log(page.page)
-        dispatchNo(no + 1)
+      if (!page) return;
+      if (data.isBottom && data.isDown && page.page < page.count) {
+        dispatch({
+          no: no + 1,
+        });
       }
-    }, 500)
+    }, 500);
   }
 
-  useEffect(function () {
-    query();
-  }, [props.file]);
+  useEffect(
+    function() {
+      query();
+    },
+    [props.file],
+  );
 
   return (
     <>
@@ -257,18 +267,18 @@ const Index = function (props: IProps) {
         contextMenu={contextMenu}
         onClickColumn={onClickColumn}
         onSelect={onSelect}
-        onClickTool={ onClickTool }
-        onToolbarSuccess={ onSuccess }
-        onScrollChange={ onScroll }
-        onScroll={ onScroll }
+        onClickTool={onClickTool}
+        onToolbarSuccess={onSuccess}
+        onScrollChange={onScroll}
+        onScroll={onScroll}
       />
       {/* 编辑文件夹 */}
       <Folder
         now={fceDate}
         id={fId}
         data={{ parentId: folder }}
-        type='update'
-        onSuccess={ onSuccess }
+        type="update"
+        onSuccess={onSuccess}
       />
       {/* 重命名 */}
       <Rename
@@ -279,10 +289,10 @@ const Index = function (props: IProps) {
       />
       {/* 移动/复制文件 */}
       <Move
-        now={ moveDate }
+        now={moveDate}
         data={{ ids: moveIds, current: folder }}
-        type={ moveType }
-        onSuccess={ onMoveSuccess }
+        type={moveType}
+        onSuccess={onMoveSuccess}
       />
     </>
   );
