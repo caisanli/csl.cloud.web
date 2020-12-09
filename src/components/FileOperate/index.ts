2f65436a225f86182c59556d093dc35ff8d90b6a
CaisanLi <18081007254@sina.cn>
2020-12-09 22:29:50 +0800

    增加接口

--- src/components/FileOperate/index.ts
+++ src/components/FileOperate/index.ts
@@ -1,6 +1,7 @@
 import Folder from './Folder';
 import Rename from './Rename';
+import Share from './Share';
 import delFolder from './deleteFolder';
 import del from './deleteFile';
 
-export { Folder, Rename, delFolder, del }
+export { Folder, Rename, Share, delFolder, del };
