47a138f3e222ebd167711c555e348c5bfc5a1a25
CaisanLi <18081007254@sina.cn>
2020-12-09 22:30:10 +0800

    优化代码

--- src/components/Layouts/head/index.tsx
+++ src/components/Layouts/head/index.tsx
@@ -1,19 +1,25 @@
 import React, { FC, useState } from 'react';
 import { Dropdown, Menu } from 'antd';
 import { DownOutlined, LogoutOutlined } from '@ant-design/icons';
-import { ConnectProps, history, UserModelState, AminModelState, connect } from 'umi';
+import {
+  ConnectProps,
+  history,
+  UserModelState,
+  AminModelState,
+  connect,
+} from 'umi';
 import { INavItem } from '@/types';
-import api from '@/api/login'
+import api from '@/api/login';
 import styles from './index.module.less';
 
 interface IProps extends ConnectProps {
-  nav: INavItem[],
-  type: 'user' | 'admin',
-  user: UserModelState,
-  admin: AminModelState
+  nav: INavItem[];
+  type: 'user' | 'admin';
+  user: UserModelState;
+  admin: AminModelState;
 }
 
-const IndexPage: FC<IProps> = (props) => {
+const IndexPage: FC<IProps> = props => {
   const { nav, type, user, admin } = props;
   const localNavActive = localStorage.getItem('topNavActive');
   const [active, setActive] = useState(localNavActive || nav[0].value);
@@ -26,12 +32,10 @@ const IndexPage: FC<IProps> = (props) => {
     history.push(item.path);
   }
   // 名称
-  const name = type === 'admin'
-        ? JSON.parse(localStorage.getItem('admin') || '{}').name
-        : JSON.parse(localStorage.getItem('user') || '{}').name;
+  const name = type === 'admin' ? admin.name : user.info.name;
   // 下拉菜单
   const dropMenu = (
-    <Menu onClick={ onClickDropdown }>
+    <Menu onClick={onClickDropdown}>
       <Menu.Item key="logout">
         <LogoutOutlined />
         退出
@@ -40,7 +44,7 @@ const IndexPage: FC<IProps> = (props) => {
   );
   // 下拉菜单点击事件
   function onClickDropdown({ key }) {
-    switch(key) {
+    switch (key) {
       case 'logout':
         logout();
         break;
@@ -48,12 +52,12 @@ const IndexPage: FC<IProps> = (props) => {
   }
   // 退出
   async function logout() {
-    if(type === 'admin') {
+    if (type === 'admin') {
       await api.adminLogout();
-      history.push('/admin/login')
+      history.push('/admin/login');
     } else {
       await api.logout();
-      history.push('/login')
+      history.push('/login');
     }
   }
   return (
@@ -76,27 +80,26 @@ const IndexPage: FC<IProps> = (props) => {
         </div>
       </div>
       <div className={styles.headRight}>
-        <Dropdown overlay={ dropMenu } trigger={['click']}>
+        <Dropdown overlay={dropMenu} trigger={['click']}>
           <div className={styles.user}>
             <img src="" className={styles.userPic} />
             <span className={styles.userName}>
-              { name } <DownOutlined />
+              {name} <DownOutlined />
             </span>
           </div>
           {/* <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
             Hover me <DownOutlined />
           </a> */}
         </Dropdown>
-
       </div>
     </div>
   );
-}
+};
 const mapStateToProps = state => {
-  return ({
+  return {
     user: state.user,
-    admin: state.admin
-  });
-}
+    admin: state.admin,
+  };
+};
 
 export default connect(mapStateToProps)(IndexPage);
