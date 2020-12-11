d83cbd4e3e1a9143b6b90517d70b7a9952335ad1
CaisanLi <18081007254@sina.cn>
2020-12-11 22:10:16 +0800

    抽离代码

--- src/components/FileContainer/index.tsx
+++ src/components/FileContainer/index.tsx
@@ -5,11 +5,31 @@ import Toolbar from './toolbar';
 import Crumb from './crumb';
 import IconList from './List/icon';
 import TableList from './List/table';
-import { IFileContainerProps } from '@/types';
+import { IContextMenu, IFileContainerProps, IToolBar } from '@/types';
 import styles from './index.module.less';
 import { bytesToSize, msToDate } from '@/utils';
+import { delFolder, del } from '@/components/FileOperate';
 import { IColumn } from '@/components/Table';
-import { FileOutlined, FolderOutlined } from '@ant-design/icons';
+import {
+  CopyOutlined,
+  DeleteOutlined,
+  DownloadOutlined,
+  EditOutlined,
+  FileOutlined,
+  FolderOutlined,
+  ScissorOutlined,
+  ShareAltOutlined,
+} from '@ant-design/icons';
+import { Folder, Rename, Share } from '../FileOperate';
+import Move from '../FileOperate/Move';
+import {
+  copyBtn,
+  delBtn,
+  downBtn,
+  moveBtn,
+  shareBtn,
+} from '@/components/FileContainer/tools';
+import fileApi from '@/api/file';
 
 interface IProps extends IFileContainerProps {}
 interface IProps extends ConnectProps {
@@ -21,6 +41,15 @@ const iconStyle = {
   color: '#40a9ff',
 };
 const IndexPage = function(props: IProps) {
+  const {
+    crumbs,
+    onScrollChange,
+    page,
+    file,
+    canCreateFolder,
+    dataSource,
+  } = props;
+  const { folder, no } = file;
   const List = props.file.style === 'icon' ? IconList : TableList;
   const [elem, setElem] = useState();
   const columns: IColumn[] = [
@@ -39,7 +68,7 @@ const IndexPage = function(props: IProps) {
       width: '50%',
       link: true,
       ellipsis: true,
-      onClickLink: onClickName,
+      onClickLink: onClickColumn,
     },
     {
       key: '02',
@@ -59,39 +88,298 @@ const IndexPage = function(props: IProps) {
     },
   ];
 
-  // 点击名称
-  function onClickName(data: any) {
-    if (props.onClickColumn) props.onClickColumn(data);
-  }
   function onRef(elem) {
     setElem(elem);
   }
-  const {
-    onClickTool,
-    crumbs,
-    tools,
-    onToolbarSuccess,
-    onScrollChange,
-    onScroll,
-    ...otherProps
-  } = props;
+
+  // 文件夹
+  const [fceDate, setFceDate] = useState<number>();
+  const [fId, setFId] = useState<string>();
+
+  // 文件
+  const [renameDate, setRenameDate] = useState<number>();
+  const [fileId, setFileId] = useState<string>();
+  const [fileName, setFileName] = useState<string>();
+  const [selected, setSelected] = useState<any[]>([]);
+
+  // 操作完成的回调
+  function onSuccess() {
+    dispatch({
+      no: 1,
+    });
+  }
+
+  // 点击项
+  function onClickColumn(data: any) {
+    if (data.parentId) {
+      // 点击了文件夹
+      dispatch({
+        folder: data.id,
+        no: 1,
+      });
+    } else {
+      // 点击了文件
+    }
+  }
+
+  // 文件夹菜单
+  const folderRightMenu: IContextMenu[] = [
+    {
+      name: '更新',
+      value: 'update',
+      icon: <EditOutlined />,
+      onClick(data: any) {
+        setFId(data.id);
+        setFceDate(Date.now());
+      },
+    },
+    {
+      name: '删除',
+      value: 'delete',
+      icon: <DeleteOutlined />,
+      onClick(data: any) {
+        delFolder(data.id, onSuccess);
+      },
+    },
+    {
+      name: '分享',
+      value: 'share',
+      icon: <ShareAltOutlined />,
+      onClick(data: any) {
+        setShareFiles('');
+        setShareFolders(data.id);
+        setShareDate(Date.now());
+      },
+    },
+  ];
+  // 文件右键菜单
+  const fileRightMenu: IContextMenu[] = [
+    {
+      name: '重命名',
+      value: 'rename',
+      icon: <EditOutlined />,
+      onClick(data: any) {
+        setFileId(data.id);
+        setFileName(data.name);
+        setRenameDate(Date.now());
+      },
+    },
+    {
+      name: '删除',
+      value: 'delete',
+      icon: <DeleteOutlined />,
+      onClick(data: any) {
+        del(data.id, '', onSuccess);
+      },
+    },
+    {
+      name: '下载',
+      value: 'download',
+      icon: <DownloadOutlined />,
+      onClick(data: any) {
+        fileApi.download(data.id);
+      },
+    },
+    {
+      name: '分享',
+      value: 'share',
+      icon: <ShareAltOutlined />,
+      onClick(data: any) {
+        setShareFolders('');
+        setShareFiles(data.id);
+        setShareDate(Date.now());
+      },
+    },
+    {
+      name: '移动到',
+      value: 'move',
+      icon: <ScissorOutlined />,
+      onClick(data: any) {
+        setMoveIds(data.id);
+        move('move');
+      },
+    },
+    {
+      name: '复制到',
+      value: 'copy',
+      icon: <CopyOutlined />,
+      onClick(data: any) {
+        setMoveIds(data.id);
+        move('copy');
+      },
+    },
+  ];
+
+  // 自定义生成右键菜单
+  const contextMenu = function(data: any): IContextMenu[] {
+    if (data.parentId) return folderRightMenu;
+    else return fileRightMenu;
+  };
+
+  // 工具栏
+  const [tools, setTools] = useState<IToolBar[]>([]);
+  // 监听选中的类型并动态设置工具栏
+  function onSelect(data: any[]) {
+    let type: number = 0;
+    for (let i = 0; i < data.length; i++) {
+      const d = data[i];
+      if (type === 3) break;
+      if (d.parentId) {
+        // 文件夹
+        type = type === 1 ? 3 : 2;
+      } else {
+        // 文件
+        type = type === 2 ? 3 : 1;
+      }
+    }
+    setSelected(data);
+    let tools: IToolBar[] = [];
+    switch (type) {
+      case 1: // 文件
+        tools = [shareBtn, downBtn, delBtn, copyBtn, moveBtn];
+        break;
+      case 2: // 文件夹
+        tools = [shareBtn, delBtn];
+        break;
+      case 3: // 文件 + 文件夹
+        tools = [shareBtn];
+        break;
+    }
+    setTools(tools);
+  }
+
+  // 获取已选择的文件、文件夹ID
+  function getSelectedIds(): { files: string; folders: string } {
+    let files: string[] = [],
+      folders: string[] = [];
+    selected.forEach(sel => {
+      if (sel.parentId) folders.push(sel.id);
+      else files.push(sel.id);
+    });
+    return { files: files.join(','), folders: folders.join(',') };
+  }
+  // 移动、复制
+  const [moveDate, setMoveDate] = useState<number>();
+  const [moveIds, setMoveIds] = useState<string>();
+  const [moveType, setMoveType] = useState<string>();
+  function move(type: string) {
+    setMoveType(type);
+    setMoveDate(Date.now());
+  }
+  function onMoveSuccess(folder: string) {
+    dispatch({
+      folder,
+    });
+  }
+
+  // 分享
+  const [shareDate, setShareDate] = useState<number>();
+  const [shareFils, setShareFiles] = useState<string>();
+  const [shareFolders, setShareFolders] = useState<string>();
+  function share() {
+    let obj = getSelectedIds();
+    setShareFiles(obj.files);
+    setShareFolders(obj.folders);
+    setShareDate(Date.now());
+  }
+
+  // 滚动刷新
+  let scrollTimer: NodeJS.Timeout;
+  function onScroll(data: any) {
+    if (scrollTimer) clearTimeout(scrollTimer);
+    scrollTimer = setTimeout(() => {
+      if (!page) return;
+      if (data.isBottom && data.isDown && page.page < page.count) {
+        dispatch({
+          no: no + 1,
+        });
+      }
+    }, 500);
+  }
+
+  // 点击工具栏
+  function onClickTool(tool: IToolBar) {
+    let idsObj = getSelectedIds();
+    switch (tool.type) {
+      case 'move':
+      case 'copy':
+        setMoveIds(idsObj.files);
+        move(tool.type);
+        break;
+      case 'delete':
+        del(idsObj.files, idsObj.folders, onSuccess);
+        break;
+      case 'share':
+        share();
+        break;
+      case 'download':
+        fileApi.download(idsObj.files);
+        break;
+    }
+  }
+  // dispatch
+  function dispatch(data: any) {
+    props.dispatch &&
+      props.dispatch({
+        type: 'file/update',
+        payload: {
+          ...props.file,
+          ...data,
+        },
+      });
+  }
+
   return (
     <>
       {/* 工具栏 */}
       <Toolbar
         tools={tools}
-        canCreateFolder
+        canCreateFolder={canCreateFolder}
         onClick={onClickTool}
-        onSuccess={onToolbarSuccess}
+        onSuccess={onSuccess}
       />
       {/* 面包屑 */}
       {props.crumbs && <Crumb crumbs={crumbs} />}
       {/* 列表 */}
       <div className={styles.list} id="list">
         <Scroll onRef={onRef} onChange={onScrollChange} onScroll={onScroll}>
-          <List {...otherProps} columns={columns} scrollSelector={elem} />
+          <List
+            onSelect={onSelect}
+            contextMenu={contextMenu}
+            columns={columns}
+            scrollSelector={elem}
+            dataSource={dataSource}
+          />
         </Scroll>
       </div>
+      {/* 编辑文件夹 */}
+      <Folder
+        now={fceDate}
+        id={fId}
+        data={{ parentId: folder }}
+        type="update"
+        onSuccess={onSuccess}
+      />
+      {/* 重命名 */}
+      <Rename
+        id={fileId}
+        now={renameDate}
+        data={{ name: fileName }}
+        onSuccess={onSuccess}
+      />
+      {/* 移动/复制文件 */}
+      <Move
+        now={moveDate}
+        data={{ ids: moveIds, current: folder }}
+        type={moveType}
+        onSuccess={onMoveSuccess}
+      />
+      {/* 分享文件 */}
+      <Share
+        now={shareDate}
+        data={{ files: shareFils, folders: shareFolders }}
+        onSuccess={onSuccess}
+      />
     </>
   );
 };
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,122 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { FileModelState, ConnectProps, connect } from 'umi';
 import FileContainer from '@/components/FileContainer';
-import {
-  Folder,
-  Rename,
-  Share,
-  delFolder,
-  del,
-} from '@/components/FileOperate';
-import {
-  ShareAltOutlined,
-  DeleteOutlined,
-  EditOutlined,
-  CopyOutlined,
-  ScissorOutlined,
-} from '@ant-design/icons';
-import { IContextMenu, ICrumbItem, IPage, IToolBar } from '@/types';
+import { ICrumbItem, IPage } from '@/types';
 import fileApi from '@/api/file';
-import Move from '@/components/FileOperate/Move';
-import {
-  copyBtn,
-  delBtn,
-  downBtn,
-  moveBtn,
-  shareBtn,
-} from '@/components/FileContainer/tools';
-
 interface IProps extends ConnectProps {
   file: FileModelState;
 }
 
 const Index = function(props: IProps) {
-  // 文件夹菜单
-  const folderRightMenu: IContextMenu[] = [
-    {
-      name: '更新',
-      value: 'update',
-      icon: <EditOutlined />,
-      onClick(data: any) {
-        setFId(data.id);
-        setFceDate(Date.now());
-      },
-    },
-    {
-      name: '删除',
-      value: 'delete',
-      icon: <DeleteOutlined />,
-      onClick(data: any) {
-        delFolder(data.id, onSuccess);
-      },
-    },
-    {
-      name: '分享',
-      value: 'share',
-      icon: <ShareAltOutlined />,
-      onClick(data: any) {
-        setShareFiles('');
-        setShareFolders(data.id);
-        setShareDate(Date.now());
-      },
-    },
-  ];
-  // 文件右键菜单
-  const fileRightMenu: IContextMenu[] = [
-    {
-      name: '重命名',
-      value: 'rename',
-      icon: <EditOutlined />,
-      onClick(data: any) {
-        setFileId(data.id);
-        setFileName(data.name);
-        setRenameDate(Date.now());
-      },
-    },
-    {
-      name: '删除',
-      value: 'delete',
-      icon: <DeleteOutlined />,
-      onClick(data: any) {
-        del(data.id, '', onSuccess);
-      },
-    },
-    {
-      name: '分享',
-      value: 'share',
-      icon: <ShareAltOutlined />,
-      onClick(data: any) {
-        setShareFolders('');
-        setShareFiles(data.id);
-        setShareDate(Date.now());
-      },
-    },
-    {
-      name: '移动到',
-      value: 'move',
-      icon: <ScissorOutlined />,
-      onClick(data: any) {
-        setMoveIds(data.id);
-        move('move');
-      },
-    },
-    {
-      name: '复制到',
-      value: 'copy',
-      icon: <CopyOutlined />,
-      onClick(data: any) {
-        setMoveIds(data.id);
-        move('copy');
-      },
-    },
-  ];
-
-  // 自定义生成右键菜单
-  const contextMenu = function(data: any): IContextMenu[] {
-    if (data.parentId) return folderRightMenu;
-    else return fileRightMenu;
-  };
-
   const [crumbs, setCrumbs] = useState<ICrumbItem[]>([]);
   const [list, setList] = useState([]);
   const [page, setPage] = useState<IPage>();
@@ -133,153 +24,11 @@ const Index = function(props: IProps) {
     } = await fileApi.query(folder, type, order, no, 10, name);
     setPage(page);
     setCrumbs(crumbs);
-    setTools([]);
     if (no === 1) {
       setList(folders.concat(files));
     } else {
       setList(list.concat(files));
     }
-  }
-  // 文件夹
-  const [fceDate, setFceDate] = useState<number>();
-  const [fId, setFId] = useState<string>();
-
-  // 文件
-  const [renameDate, setRenameDate] = useState<number>();
-  const [fileId, setFileId] = useState<string>();
-  const [fileName, setFileName] = useState<string>();
-  const [selected, setSelected] = useState<any[]>([]);
-  // 操作完成的回调
-  function onSuccess() {
-    dispatch({
-      no: 1,
-    });
-  }
-
-  // dispatch
-  function dispatch(data: any) {
-    props.dispatch &&
-      props.dispatch({
-        type: 'file/update',
-        payload: {
-          ...props.file,
-          ...data,
-        },
-      });
-  }
-  // 点击项
-  function onClickColumn(data: any) {
-    if (data.parentId) {
-      // 点击了文件夹
-      dispatch({
-        folder: data.id,
-        no: 1,
-      });
-    } else {
-      // 点击了文件
-    }
-  }
-
-  // 工具栏
-  const [tools, setTools] = useState<IToolBar[]>([]);
-  // 监听选中
-  function onSelect(data: any[]) {
-    let type: number = 0;
-
-    for (let i = 0; i < data.length; i++) {
-      const d = data[i];
-      if (type === 3) break;
-      if (d.parentId) {
-        // 文件夹
-        type = type === 1 ? 3 : 2;
-      } else {
-        // 文件
-        type = type === 2 ? 3 : 1;
-      }
-    }
-    setSelected(data);
-    let tools: IToolBar[] = [];
-    switch (type) {
-      case 1: // 文件
-        tools = [shareBtn, downBtn, delBtn, copyBtn, moveBtn];
-        break;
-      case 2: // 文件夹
-        tools = [shareBtn, delBtn];
-        break;
-      case 3: // 文件 + 文件夹
-        tools = [shareBtn];
-        break;
-    }
-    setTools(tools);
-  }
-  // 点击工具栏
-  function onClickTool(tool: IToolBar) {
-    let idsObj = getSelectedIds();
-    switch (tool.type) {
-      case 'move':
-      case 'copy':
-        setMoveIds(idsObj.files);
-        move(tool.type);
-        break;
-      case 'delete':
-        del(idsObj.files, idsObj.folders, onSuccess);
-        break;
-      case 'share':
-        share();
-        break;
-      case 'download':
-        fileApi.download(idsObj.files);
-        break;
-    }
-  }
-  // 获取
-  function getSelectedIds(): { files: string; folders: string } {
-    let files: string[] = [],
-      folders: string[] = [];
-    selected.forEach(sel => {
-      if (sel.parentId) folders.push(sel.id);
-      else files.push(sel.id);
-    });
-    return { files: files.join(','), folders: folders.join(',') };
-  }
-  // 移动、复制
-  const [moveDate, setMoveDate] = useState<number>();
-  const [moveIds, setMoveIds] = useState<string>();
-  const [moveType, setMoveType] = useState<string>();
-  function move(type: string) {
-    setMoveType(type);
-    setMoveDate(Date.now());
-  }
-
-  function onMoveSuccess(folder: string) {
-    dispatch({
-      folder,
-    });
-  }
-
-  // 分享
-  const [shareDate, setShareDate] = useState<number>();
-  const [shareFils, setShareFiles] = useState<string>();
-  const [shareFolders, setShareFolders] = useState<string>();
-
-  function share() {
-    let obj = getSelectedIds();
-    setShareFiles(obj.files);
-    setShareFolders(obj.folders);
-    setShareDate(Date.now());
-  }
-
-  let scrollTimer: NodeJS.Timeout;
-  function onScroll(data: any) {
-    if (scrollTimer) clearTimeout(scrollTimer);
-    scrollTimer = setTimeout(() => {
-      if (!page) return;
-      if (data.isBottom && data.isDown && page.page < page.count) {
-        dispatch({
-          no: no + 1,
-        });
-      }
-    }, 500);
   }
 
   useEffect(
@@ -296,42 +45,7 @@ const Index = function(props: IProps) {
         type="person"
         canCreateFolder
         crumbs={crumbs}
-        tools={tools}
-        contextMenu={contextMenu}
-        onClickColumn={onClickColumn}
-        onSelect={onSelect}
-        onClickTool={onClickTool}
-        onToolbarSuccess={onSuccess}
-        onScrollChange={onScroll}
-        onScroll={onScroll}
-      />
-      {/* 编辑文件夹 */}
-      <Folder
-        now={fceDate}
-        id={fId}
-        data={{ parentId: folder }}
-        type="update"
-        onSuccess={onSuccess}
-      />
-      {/* 重命名 */}
-      <Rename
-        id={fileId}
-        now={renameDate}
-        data={{ name: fileName }}
-        onSuccess={onSuccess}
-      />
-      {/* 移动/复制文件 */}
-      <Move
-        now={moveDate}
-        data={{ ids: moveIds, current: folder }}
-        type={moveType}
-        onSuccess={onMoveSuccess}
-      />
-      {/* 分享文件 */}
-      <Share
-        now={shareDate}
-        data={{ files: shareFils, folders: shareFolders }}
-        onSuccess={onSuccess}
+        page={page}
       />
     </>
   );
