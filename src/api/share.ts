a881ca1684ea6d21011d990a82fcf35377676710
CaisanLi <18081007254@sina.cn>
2020-12-13 22:12:57 +0800

    增加分享模块的接口

--- src/api/share.ts
+++ src/api/share.ts
@@ -11,4 +11,26 @@ export default {
   create(users: string, files: string, folders: string) {
     return post('/share', { users, files, folders });
   },
+  /**
+   * 获取我的分享
+   * @returns
+   */
+  query() {
+    return get('/share');
+  },
+  /**
+   * 获取分享给我的
+   * @returns
+   */
+  queryToMe() {
+    return get('/share/toMe');
+  },
+  /**
+   * 获取分享的详情（文件/文件夹）
+   * @param {string} id
+   * @returns
+   */
+  detail(id: string) {
+    return get('/share/' + id + '/detail');
+  },
 };
