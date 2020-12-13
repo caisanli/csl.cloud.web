bd7d59df5f06408670e0b3ec63d3266d8f3e717a
CaisanLi <18081007254@sina.cn>
2020-12-13 22:14:55 +0800

    优化代码

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -8,89 +8,89 @@ import { message } from 'antd';
 interface IProps extends ConnectProps {
   user: UserModelState;
 }
-const fieldData: IFieldProps[] = [{
-  key: 'name',
-  label: '名称',
-  rules: [
-    { required: true },
-    { type: 'string', min: 2, max: 24 }
-  ],
-  input: {
-    placeholder: '输入用户名'
-  }
-}, {
-  key: 'password',
-  label: '密码',
-  rules: [
-    { required: true },
-    { type: 'string', min: 2 }
-  ],
-  input: {
-    customType: 'password',
-    placeholder: '输入密码'
-  }
-},
-// {
-//   key: 'remember',
-//   style: {
-//     textAlign: 'right'
-//   },
-//   valuePropName: 'remember',
-//   input: {
-//     customType: 'checkbox',
-//     label: '记住我'
-//   }
-// },
-{
-  key: 'submit',
-  style: {
-    textAlign: 'center'
+const fieldData: IFieldProps[] = [
+  {
+    key: 'name',
+    label: '名称',
+    rules: [{ required: true }, { type: 'string', min: 2, max: 24 }],
+    input: {
+      placeholder: '输入用户名',
+    },
   },
-  input: {
-    type: 'primary',
-    label: '登录',
-    customType: 'button',
-    htmlType: 'submit',
+  {
+    key: 'password',
+    label: '密码',
+    rules: [{ required: true }, { type: 'string', min: 2 }],
+    input: {
+      customType: 'password',
+      placeholder: '输入密码',
+    },
+  },
+  // {
+  //   key: 'remember',
+  //   style: {
+  //     textAlign: 'right'
+  //   },
+  //   valuePropName: 'remember',
+  //   input: {
+  //     customType: 'checkbox',
+  //     label: '记住我'
+  //   }
+  // },
+  {
+    key: 'submit',
     style: {
-      width: '100%'
-    }
-  }
-}];
-const IndexPage:FC<IProps> = ({ dispatch }) => {
-  // const [ form ] = Form.useForm();
+      textAlign: 'center',
+    },
+    input: {
+      type: 'primary',
+      label: '登录',
+      customType: 'button',
+      htmlType: 'submit',
+      style: {
+        width: '100%',
+      },
+    },
+  },
+];
+const IndexPage: FC<IProps> = ({ dispatch }) => {
   let formIns: IFormInstance;
   function onFinish(values: any) {
-    console.log(values)
-    api.login(values.name, values.password).then(({ data }) => {
-      message.success('登录成功');
-      dispatch && dispatch({
-        type: 'user/updateInfo',
-        payload: {
-          info: data
-        }
+    api
+      .login(values.name, values.password)
+      .then(({ data }) => {
+        message.success('登录成功');
+        dispatch &&
+          dispatch({
+            type: 'user/updateInfo',
+            payload: {
+              info: data,
+            },
+          });
+        localStorage.setItem('topNavActive', '');
+        localStorage.setItem('asideNavActive', '');
+        localStorage.setItem('user', JSON.stringify(data));
+        history.push('/');
       })
-      localStorage.setItem('topNavActive', '');
-      localStorage.setItem('user', JSON.stringify(data));
-      history.push('/');
-    }).catch(() => {
-      // 清空信息
-      formIns.setFieldsValue({
-        password: '',
-        name: ''
-      })
-    })
+      .catch(() => {
+        // 清空信息
+        formIns.setFieldsValue({
+          password: '',
+          name: '',
+        });
+      });
   }
 
   return (
-    <div className={ styles.login }>
+    <div className={styles.login}>
       <Form
-        className={ styles.loginForm }
-        labelCol = {{ span: 5 }}
-        fieldData = { fieldData }
-        ref = { (form: IFormInstance) => formIns = form }
-        onFinish = { onFinish }
+        className={styles.loginForm}
+        labelCol={{ span: 5 }}
+        fieldData={fieldData}
+        ref={(form: IFormInstance) => (formIns = form)}
+        onFinish={onFinish}
       />
     </div>
   );
-}
+};
 export default connect()(IndexPage);
