59b81b09ae7c4d14522d2ad5761799a648613e22
CaisanLi <18081007254@sina.cn>
2020-12-13 22:15:28 +0800

    新增分享页面

--- src/pages/index/share.tsx
+++ src/pages/index/share.tsx
@@ -1,17 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tabs } from 'antd';
+import { history } from 'umi';
+import api from '@/api/share';
 import styles from './share.module.less';
+import { IShare } from '@/types';
+import Table, { IColumn } from '@/components/Table';
+import { msToDate } from '@/utils';
 const { TabPane } = Tabs;
 
 export default function() {
-  function callback(key) {}
+  const columns: IColumn[] = [
+    {
+      key: 'name',
+      value: 'name',
+      link: true,
+      onClickLink(data: any) {
+        history.push({
+          pathname: '/index/share/' + data.id,
+          state: {
+            id: data.id,
+          },
+        });
+        // history.push('/index/share/' + data.id)
+      },
+      label: '名称',
+      ellipsis: true,
+      width: '50%',
+    },
+    {
+      key: 'date',
+      label: '分享时间',
+      render(data: any) {
+        return <>{msToDate(data.date)}</>;
+      },
+    },
+    {
+      key: 'count',
+      label: '文件/文件夹',
+      render(data: any) {
+        return <>{data.count + '个'}</>;
+      },
+    },
+  ];
+  const [list, setList] = useState<IShare[]>([]);
+  function callback(key: string) {
+    switch (key) {
+      case '1':
+        query1();
+        break;
+      case '2':
+        query2();
+        break;
+    }
+  }
+  useEffect(() => {
+    query1();
+  }, []);
+
+  async function query1() {
+    const { data } = await api.query();
+    setList(data);
+  }
+
+  async function query2() {
+    const { data } = await api.queryToMe();
+    setList(data);
+  }
+
   return (
     <Tabs className={styles.share} defaultActiveKey="1" onChange={callback}>
       <TabPane tab="我的分享" key="1">
-        我的分享
+        <Table dataIndex="id" columns={columns} dataSource={list} />
       </TabPane>
       <TabPane tab="分享给我" key="2">
-        分享给我
+        <Table dataIndex="id" columns={columns} dataSource={list} />
       </TabPane>
     </Tabs>
   );
---
+++ src/pages/index/share/[id].tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'umi';
+import { Row, Col } from 'antd';
+import Crumb from '@/components/Crumb';
+import Scroll from '@/components/Scroll';
+import shareApi from '@/api/share';
+import fileApi from '@/api/file';
+import { ICrumbItem, IPage } from '@/types';
+import Table, { IColumn } from '@/components/Table';
+import { FileOutlined, FolderOutlined } from '@ant-design/icons';
+import { bytesToSize, msToDate } from '@/utils';
+const iconStyle = {
+  fontSize: '18px',
+  verticalAlign: 'sub',
+  color: '#40a9ff',
+};
+export default function() {
+  const params = useParams<{id: string}>()
+  const [param, setParam] = useState<{folder: string, no: number}>({folder: '0', no: 1});
+  const [list, setList] = useState([]);
+  const [crumbs, setCrumbs] = useState<ICrumbItem[]>([]);
+  const [page, setPage] = useState<IPage>({page: 1, total: 0, count: 0});
+  const columns: IColumn[] = [
+    {
+      key: '00',
+      width: 40,
+      render(data: any) {
+        if (data.parentId) return <FolderOutlined style={iconStyle} />;
+        return <FileOutlined style={iconStyle} />;
+      },
+    },
+    {
+      key: '01',
+      label: '文件名',
+      value: 'name',
+      width: '50%',
+      link: true,
+      ellipsis: true,
+      onClickLink: onClickColumn,
+    },
+    {
+      key: '02',
+      label: '大小',
+      value: 'size',
+      render(data: any) {
+        return <>{bytesToSize(data.size)}</>;
+      },
+    },
+    {
+      key: '03',
+      label: '修改日期',
+      value: 'modifyDate',
+      render(data: any) {
+        return <>{msToDate(data.modifyDate)}</>;
+      },
+    },
+  ];
+
+  function onClickColumn(data: any) {
+    if(data.parentId) {
+      setParam({
+        no: 1,
+        folder: data.id
+      })
+    }
+
+  }
+
+  useEffect(() => {
+    query();
+  }, [param])
+
+  // 查询
+  async function query() {
+    let { folder, no } = param;
+    if(folder === '0') {
+      let { data: { files, folders } } = await shareApi.detail(params.id)
+      setList(folders.concat(files));
+      setCrumbs([]);
+    } else {
+      let { data } = await fileApi.query('name', 'ASC', no, 100, '', folder);
+      let { files, folders, crumbs, page } = data;
+      if(page.page === 1) {
+        setList(folders.concat(files))
+      } else {
+        setList(list.concat(files))
+      }
+      setCrumbs(crumbs);
+      setPage(page);
+    }
+  }
+
+  function onClick(item: ICrumbItem) {
+    setParam({
+      no: 1,
+      folder: item.id
+    })
+  }
+
+  return (
+    <>
+      <Row justify="space-between" align="middle">
+        <Col>
+          <Crumb
+            crumbs={crumbs}
+            onClick={onClick}
+          />
+        </Col>
+        <Col style={{paddingRight: '10px'}}>
+          共{ list.length }项
+        </Col>
+      </Row>
+
+      <Scroll>
+        <Table
+          dataIndex='id'
+          columns={columns}
+          dataSource={list}
+        />
+      </Scroll>
+
+    </>
+  )
+}
