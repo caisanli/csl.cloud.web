a6c6067569a8a44b5be23c5d0acb3dc3ef401632
CaisanLi <18081007254@sina.cn>
2021-05-05 15:47:35 +0800

    style(*):调整代码格式

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import FileContainer from '@/components/FileContainer';
 
 const Index = function() {
-  return (
-    <>
-      <FileContainer type="person" canCreateFolder showFolder />
-    </>
-  );
+  return <FileContainer type="person" canCreateFolder showFolder />;
 };
 
 export default Index;
